521a8aa6b7462851a9afbc679e64781e5d965578
Alex Horobets <sgs.cat.fly@gmail.com>
2023-11-02 20:55:27 +0200

    refactored code

--- src/main/java/com/ip12/currencycrafter/repository/CurrencyRepository.java
+++ src/main/java/com/ip12/currencycrafter/repository/CurrencyRepository.java
@@ -3,8 +3,6 @@ package com.ip12.currencycrafter.repository;
 
 import com.ip12.currencycrafter.entity.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
 }
--- src/main/java/com/ip12/currencycrafter/repository/ExchangeRateRepository.java
+++ src/main/java/com/ip12/currencycrafter/repository/ExchangeRateRepository.java
@@ -4,17 +4,11 @@ package com.ip12.currencycrafter.repository;
 import com.ip12.currencycrafter.entity.Currency;
 import com.ip12.currencycrafter.entity.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
-//    ExchangeRate findByCurrencyAndDate(Currency currency, Date date);
-//
-//    List<ExchangeRate> findAllByCurrencyOrderByDateDesc(Currency currency);
-
     List<ExchangeRate> findAllByCurrency_Id(Long currencyId);
 
     List<ExchangeRate> findAllByCurrency(Currency currency);

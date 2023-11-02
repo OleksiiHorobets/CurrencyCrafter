a1424d254f61b651784e31001535bdf6401d2e9e
Alex Horobets <sgs.cat.fly@gmail.com>
2023-11-02 16:52:35 +0200

    refactored code

--- src/main/java/com/ip12/currencycrafter/entity/Currency.java
+++ src/main/java/com/ip12/currencycrafter/entity/Currency.java
@@ -1,7 +1,11 @@
 package com.ip12.currencycrafter.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;

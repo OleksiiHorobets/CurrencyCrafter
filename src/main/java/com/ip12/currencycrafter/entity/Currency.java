3a11898c31939c3b750c57af7717ef3510a0d833
Alex Horobets <sgs.cat.fly@gmail.com>
2023-11-02 17:24:54 +0200

    added Currency view

--- src/main/java/com/ip12/currencycrafter/entity/Currency.java
+++ src/main/java/com/ip12/currencycrafter/entity/Currency.java
@@ -1,11 +1,7 @@
 package com.ip12.currencycrafter.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;

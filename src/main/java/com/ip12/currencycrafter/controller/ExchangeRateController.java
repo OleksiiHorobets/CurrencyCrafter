2b189d21338f880167d13496b18087f716f5007e
Oleksii Horobets <90183750+OleksiiHorobets@users.noreply.github.com>
2023-12-09 19:12:22 +0200

    added CurrencyRateInfo dto, refactored services for it (#7)

--- src/main/java/com/ip12/currencycrafter/controller/ExchangeRateController.java
+++ src/main/java/com/ip12/currencycrafter/controller/ExchangeRateController.java
@@ -1,16 +1,12 @@
 package com.ip12.currencycrafter.controller;
 
-import com.ip12.currencycrafter.entity.ExchangeRate;
 import com.ip12.currencycrafter.service.CurrencyService;
 import com.ip12.currencycrafter.service.ExchangeRateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,15 +27,15 @@ public class ExchangeRateController {
         return "add-exchange-rate";
     }
 
-    @ResponseBody
-    @PostMapping("/currencies/{currencyId}/exchange-rates")
-    public ResponseEntity<?> addExchangeRate(@PathVariable("currencyId") Long currencyId, @RequestBody ExchangeRate exchangeRateDto) {
-        if (exchangeRateDto.getId() != null) {
-            return ResponseEntity.badRequest().build();
-        }
-        exchangeRateDto.setCurrency(currencyService.getById(currencyId));
-        ExchangeRate exchangeRate = exchangeRateService.save(exchangeRateDto);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(exchangeRate.getId()).toUri();
-        return ResponseEntity.created(uri).body(exchangeRate);
-    }
+//    @ResponseBody
+//    @PostMapping("/currencies/{currencyId}/exchange-rates")
+//    public ResponseEntity<?> addExchangeRate(@PathVariable("currencyId") Long currencyId, @RequestBody ExchangeRateDto exchangeRateDto) {
+//        if (exchangeRateDto.getId() != null) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//        exchangeRateDto.setCurrency(currencyService.getById(currencyId));
+//        ExchangeRate exchangeRate = exchangeRateService.save(exchangeRateDto);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(exchangeRate.getId()).toUri();
+//        return ResponseEntity.created(uri).body(exchangeRate);
+//    }
 }

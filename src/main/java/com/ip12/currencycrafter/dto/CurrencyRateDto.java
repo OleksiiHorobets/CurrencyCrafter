5730237952b448a74b15377c3df44f6cdd7c5340
anna-kushnir <anna.kushnir.1008@gmail.com>
2023-12-11 19:02:17 +0200

    added swagger documentation for currency rest controller

--- src/main/java/com/ip12/currencycrafter/dto/CurrencyRateDto.java
+++ src/main/java/com/ip12/currencycrafter/dto/CurrencyRateDto.java
@@ -1,12 +1,16 @@
 package com.ip12.currencycrafter.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class CurrencyRateDto {
+    @Schema(description = "Currency Id", example = "5")
     private Long id;
+    @Schema(description = "Currency Name", example = "UAH")
     private String name;
+    @Schema(description = "Currency Exchange Rates")
     private List<ExchangeRateDto> exchangeRates;
 }

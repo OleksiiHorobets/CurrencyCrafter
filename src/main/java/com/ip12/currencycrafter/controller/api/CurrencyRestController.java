e7e3fd3793998b2a883687ad188adbd32cf595e2
Alex Horobets <sgs.cat.fly@gmail.com>
2023-12-14 21:10:47 +0200

    changed jdbcTemplate query

--- src/main/java/com/ip12/currencycrafter/controller/api/CurrencyRestController.java
+++ src/main/java/com/ip12/currencycrafter/controller/api/CurrencyRestController.java
@@ -4,14 +4,15 @@ import com.ip12.currencycrafter.dto.CurrencyRateDto;
 import com.ip12.currencycrafter.dto.ExchangeRateDto;
 import com.ip12.currencycrafter.exception.ResourceNotFoundException;
 import com.ip12.currencycrafter.service.CurrencyService;
-import com.ip12.currencycrafter.service.ExchangeRateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 @RequestMapping("/api/v1/currencies")
@@ -19,7 +20,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CurrencyRestController {
     private final CurrencyService currencyService;
-    private final ExchangeRateService exchangeRateService;
     private final JdbcTemplate jdbcTemplate;
 
     @GetMapping
@@ -33,23 +33,29 @@ public class CurrencyRestController {
         return ResponseEntity.ok(currencyRateInfo);
     }
 
-    @GetMapping("today/uah")
+    @GetMapping("/today/uah")
     public ResponseEntity<ExchangeRateDto> getCurrentUahRateToUsd() throws ResourceNotFoundException {
-        String sql = "SELECT ID FROM CURRENCY_SCHEMA.EXCHANGE_RATE WHERE CURRENCY_ID = " +
-                "(SELECT ID FROM CURRENCY_SCHEMA.CURRENCY WHERE NAME = 'UAH') AND LOCAL_DATE = CURRENT_DATE";
-        int exchangeRateId;
-        try {
-            exchangeRateId = jdbcTemplate.queryForObject(
-                    sql,
-                    Integer.class
-            );
-        }
-        catch (ResourceNotFoundException e) {
-            return ResponseEntity.badRequest().build();
-        }
+        String sql = """
+                SELECT exchange_rate.id, local_date, rate
+                FROM CURRENCY_SCHEMA.EXCHANGE_RATE as exchange_rate
+                         LEFT JOIN CURRENCY_SCHEMA.CURRENCY as currency on exchange_rate.currency_id = currency.id
+                WHERE currency.name = 'UAH'
+                  and exchange_rate.local_date = CURRENT_DATE
+                 """;
 
-        ExchangeRateDto exchangeRateInfo = exchangeRateService.getById(exchangeRateId);
-        return ResponseEntity.ok(exchangeRateInfo);
+        var exchangeRateDto = jdbcTemplate.queryForObject(
+                sql,
+                mapResultSetToExchangeRateDto()
+        );
+        return ResponseEntity.ok(exchangeRateDto);
+    }
+
+    private RowMapper<ExchangeRateDto> mapResultSetToExchangeRateDto() {
+        return (rs, rowNum) -> ExchangeRateDto.builder()
+                .id(rs.getLong("id"))
+                .localDate(rs.getObject("local_date", LocalDate.class))
+                .rate(rs.getBigDecimal("rate"))
+                .build();
     }
 
     @DeleteMapping("/{id}")

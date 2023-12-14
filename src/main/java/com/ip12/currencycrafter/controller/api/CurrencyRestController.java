563d3a08e223981a5c104f290fa4878da51e5294
Hanna Kushnir <62173987+anna-kushnir@users.noreply.github.com>
2023-12-14 21:11:36 +0200

    added get method in CurrencyRestController which returns current UAH rate to USD (#18)
    * added get method in CurrencyRestController
    
    * added some seed data
    
    * changed jdbcTemplate query

--- src/main/java/com/ip12/currencycrafter/controller/api/CurrencyRestController.java
+++ src/main/java/com/ip12/currencycrafter/controller/api/CurrencyRestController.java
@@ -1,14 +1,18 @@
 package com.ip12.currencycrafter.controller.api;
 
 import com.ip12.currencycrafter.dto.CurrencyRateDto;
+import com.ip12.currencycrafter.dto.ExchangeRateDto;
 import com.ip12.currencycrafter.exception.ResourceNotFoundException;
 import com.ip12.currencycrafter.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.util.List;
 
 @RequestMapping("/api/v1/currencies")
@@ -16,6 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CurrencyRestController {
     private final CurrencyService currencyService;
+    private final JdbcTemplate jdbcTemplate;
 
     @GetMapping
     public List<CurrencyRateDto> findAll() {
@@ -26,6 +31,31 @@ public class CurrencyRestController {
     public ResponseEntity<CurrencyRateDto> findById(@PathVariable Long id) throws ResourceNotFoundException {
         CurrencyRateDto currencyRateInfo = currencyService.getById(id);
         return ResponseEntity.ok(currencyRateInfo);
+    }
+
+    @GetMapping("/today/uah")
+    public ResponseEntity<ExchangeRateDto> getCurrentUahRateToUsd() throws ResourceNotFoundException {
+        String sql = """
+                SELECT exchange_rate.id, local_date, rate
+                FROM CURRENCY_SCHEMA.EXCHANGE_RATE as exchange_rate
+                         LEFT JOIN CURRENCY_SCHEMA.CURRENCY as currency on exchange_rate.currency_id = currency.id
+                WHERE currency.name = 'UAH'
+                  and exchange_rate.local_date = CURRENT_DATE
+                 """;
+
+        var exchangeRateDto = jdbcTemplate.queryForObject(
+                sql,
+                mapResultSetToExchangeRateDto()
+        );
+        return ResponseEntity.ok(exchangeRateDto);
+    }
+
+    private RowMapper<ExchangeRateDto> mapResultSetToExchangeRateDto() {
+        return (rs, rowNum) -> ExchangeRateDto.builder()
+                .id(rs.getLong("id"))
+                .localDate(rs.getObject("local_date", LocalDate.class))
+                .rate(rs.getBigDecimal("rate"))
+                .build();
     }
 
     @DeleteMapping("/{id}")
